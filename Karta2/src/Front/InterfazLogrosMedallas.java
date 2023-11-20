4655442f80e11d7e51e9c14f5015cba275ac67be
Isis <Isis@LAPTOP-4PSVTE46>
2023-11-20 15:46:42 -0600

    subiendo1...

--- Karta2/src/Front/InterfazLogrosMedallas.java
+++ Karta2/src/Front/InterfazLogrosMedallas.java
@@ -4,17 +4,26 @@
  */
 package Front;
 
+import java.util.ArrayList;
+import javax.swing.JTextField;
+
 /**
  *
  * @author nisit
  */
 public class InterfazLogrosMedallas extends javax.swing.JFrame {
-
+    String nombre,passw;
+    String datos="";
+    
+    void guardarUsuario(String nombre, String passw){
+        
+    }
     /**
      * Creates new form Iniciar
      */
-    public InterfazLogrosMedallas() {
+    public InterfazLogrosMedallas(String nombre, String passw) {
         initComponents();
+        
     }
 
     /**
@@ -26,32 +35,220 @@ public class InterfazLogrosMedallas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        Panelg = new javax.swing.JPanel();
+        ftper = new javax.swing.JLabel();
+        editPerfBtn = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        logrostxt = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        medallastxt = new javax.swing.JLabel();
+        oroBB = new javax.swing.JLabel();
+        plataBB = new javax.swing.JLabel();
+        bonceBB = new javax.swing.JLabel();
+        orotxt = new javax.swing.JLabel();
+        platatxt = new javax.swing.JLabel();
+        broncetxt = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        NOMUS = new javax.swing.JTextField();
+
+        jLabel6.setText("jLabel6");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(144, 215, 155));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(144, 215, 155));
-        jPanel1.setForeground(new java.awt.Color(144, 215, 155));
-        jPanel1.setPreferredSize(new java.awt.Dimension(825, 480));
+        Panelg.setBackground(new java.awt.Color(144, 215, 155));
+        Panelg.setForeground(new java.awt.Color(144, 215, 155));
+        Panelg.setPreferredSize(new java.awt.Dimension(825, 480));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 830, Short.MAX_VALUE)
+        ftper.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Ellipse 1.png"))); // NOI18N
+
+        editPerfBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Group 3.png"))); // NOI18N
+        editPerfBtn.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        editPerfBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 153));
+        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel3.setPreferredSize(new java.awt.Dimension(164, 36));
+
+        logrostxt.setBackground(new java.awt.Color(255, 255, 255));
+        logrostxt.setFont(new java.awt.Font("Arial Black", 2, 12)); // NOI18N
+        logrostxt.setText("LOGROS");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(54, Short.MAX_VALUE)
+                .addComponent(logrostxt)
+                .addGap(52, 52, 52))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(logrostxt)
+                .addGap(22, 22, 22))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 830, 480));
+        jPanel4.setBackground(new java.awt.Color(255, 255, 153));
+        jPanel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel4.setPreferredSize(new java.awt.Dimension(164, 36));
+
+        medallastxt.setBackground(new java.awt.Color(255, 255, 255));
+        medallastxt.setFont(new java.awt.Font("Arial Black", 2, 12)); // NOI18N
+        medallastxt.setText("MEDALLAS");
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addComponent(medallastxt)
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(medallastxt)
+                .addGap(22, 22, 22))
+        );
+
+        oroBB.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Group 24.png"))); // NOI18N
+
+        plataBB.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Group 23.png"))); // NOI18N
+
+        bonceBB.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Group 22.png"))); // NOI18N
+
+        orotxt.setText("0");
+
+        platatxt.setText("0");
+
+        broncetxt.setText("0");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 3, 16)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(153, 0, 0));
+        jLabel1.setText("Cerrar Secion");
+        jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imgs/Group 36.png"))); // NOI18N
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        NOMUS.setText(nombre);
+        NOMUS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NOMUSActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout PanelgLayout = new javax.swing.GroupLayout(Panelg);
+        Panelg.setLayout(PanelgLayout);
+        PanelgLayout.setHorizontalGroup(
+            PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PanelgLayout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(36, 36, 36))
+            .addGroup(PanelgLayout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(102, 102, 102)
+                .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(PanelgLayout.createSequentialGroup()
+                        .addComponent(ftper)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
+                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14))
+                    .addGroup(PanelgLayout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(NOMUS)
+                        .addGap(18, 18, 18)
+                        .addComponent(editPerfBtn)
+                        .addGap(129, 129, 129)
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(oroBB)
+                            .addComponent(bonceBB)
+                            .addComponent(plataBB))
+                        .addGap(65, 65, 65)
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(platatxt, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(orotxt, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(broncetxt, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(37, 37, 37))
+        );
+        PanelgLayout.setVerticalGroup(
+            PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PanelgLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(12, 12, 12)
+                .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PanelgLayout.createSequentialGroup()
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(16, 16, 16)
+                                .addComponent(ftper))
+                            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(editPerfBtn)
+                            .addComponent(NOMUS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(PanelgLayout.createSequentialGroup()
+                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(37, 37, 37)
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(oroBB)
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(25, 25, 25)
+                                .addComponent(orotxt)))
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(plataBB))
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(41, 41, 41)
+                                .addComponent(platatxt)))
+                        .addGroup(PanelgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(bonceBB))
+                            .addGroup(PanelgLayout.createSequentialGroup()
+                                .addGap(38, 38, 38)
+                                .addComponent(broncetxt)))))
+                .addContainerGap(54, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(Panelg, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 830, 480));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        NOMUS.setText(datos);
+        
+        InterfazIniciarCrearPerfil obj= new InterfazIniciarCrearPerfil();
+        obj.setVisible(true);
+        this.hide();
+    }//GEN-LAST:event_jLabel1MouseClicked
+
+    private void NOMUSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NOMUSActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_NOMUSActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,16 +277,37 @@ public class InterfazLogrosMedallas extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InterfazLogrosMedallas().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField NOMUS;
+    private javax.swing.JPanel Panelg;
+    private javax.swing.JLabel bonceBB;
+    private javax.swing.JLabel broncetxt;
+    private javax.swing.JLabel editPerfBtn;
+    private javax.swing.JLabel ftper;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JLabel logrostxt;
+    private javax.swing.JLabel medallastxt;
+    private javax.swing.JLabel oroBB;
+    private javax.swing.JLabel orotxt;
+    private javax.swing.JLabel plataBB;
+    private javax.swing.JLabel platatxt;
     // End of variables declaration//GEN-END:variables
 }
