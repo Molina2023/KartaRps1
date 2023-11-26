5039482463830851c4e8804d157773b2c651ce92
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-26 01:38:27 -0600

    un main random para probar el código

---
+++ Karta2/src/karta2/RandomAll.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karta2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nisit
+ */
+public class RandomAll {
+
+    public static void main(String[] args) {
+        ArrayList<Preguntas> array = new ArrayList<Preguntas>();
+        ElementosObj obj = new ElementosObj();
+        
+        array = obj.DeterminarPregunta(10);
+    }
+    
+    
+}
