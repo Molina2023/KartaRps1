58467affbf2ccb0862c440f5fce2115a6481ddb4
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-26 01:36:53 -0600

    Aquí se meten los objetos/elementos again

--- Karta2/src/karta2/ElementosObj.java
+++ Karta2/src/karta2/ElementosObj.java
@@ -12,11 +12,30 @@ public class ElementosObj {
     
     
     public ElementosObj (){
-    
-    }
-    void Load (){
         arrayEle.add(new Elemento (1, "H", "el Hidrógeno", Periodo.PERIODO1, Grupo.IA, "gas", 1.008, 2.1));
-        
+        arrayEle.add(new Elemento (1, "H", "el Hidrógeno", Periodo.PERIODO1, Grupo.IA, "gas", 1.008, 2.1));
     }
+    
+    private Preguntas DeterminarElemento(){
+        int numElemento = (int) (Math.random() * 0);
+        int tipoPregunta = (int) (Math.random() * 6);
+        System.out.println(tipoPregunta);
+        Preguntas item = new Preguntas();
+        item.ElementFind = arrayEle.get(0);
+        item.QuestionType = Pregunta.values()[tipoPregunta];
+        return item;
+    }
+    
+    public ArrayList<Preguntas> DeterminarPregunta (int numPreguntas) {
+        ArrayList<Preguntas> item = new ArrayList<Preguntas>();
+        
+        for (int i = 0; i < numPreguntas; i++) {
+            item.add(DeterminarElemento());
+            System.out.println(item.get(i).getElementFind());
+            System.out.println(item.get(i).getQuestionType());
+        }
+        return item;
+    }
+    
     
 }
