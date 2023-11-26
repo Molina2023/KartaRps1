56566176cdae363c4c9430141b4a5e7de6f93aa5
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-26 01:38:08 -0600

    Clase tipo de pregunta + elemento

---
+++ Karta2/src/karta2/Preguntas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karta2;
+
+public class Preguntas {
+    Elemento ElementFind;
+    Pregunta QuestionType;
+
+    public Elemento getElementFind() {
+        return ElementFind;
+    }
+
+    public Pregunta getQuestionType() {
+        return QuestionType;
+    }
+    
+    
+}
