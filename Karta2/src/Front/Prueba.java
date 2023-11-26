a4dc27f84c373bd14db086306948e1f494fc45a0
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-25 21:22:08 -0600

    Imagenes again

---
+++ Karta2/src/Front/Prueba.java
@@ -0,0 +1,18 @@
+package Front;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nisit
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        InterfazMenú obj = new InterfazMenú();
+        //obj.setSize((int) pantalla.getWidth(), 0);
+        
+    }
+}
