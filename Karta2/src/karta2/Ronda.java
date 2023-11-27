945a5fcbbc7dd6a9ac772d9f6a8a20aad94986d2
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-27 00:12:34 -0600

    Clase de Ronda con un array de preguntas

---
+++ Karta2/src/karta2/Ronda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karta2;
+
+import java.util.ArrayList;
+
+public class Ronda {
+    //Usuario usuario;
+    ArrayList<PreguntaRespuesta> arrayPreguntas = new ArrayList<PreguntaRespuesta>();
+    Enum.Medalla Medallas;
+}
