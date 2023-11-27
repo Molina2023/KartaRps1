bf36f7f048b754bbe16454f61d955b3305098cd7
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-27 00:11:36 -0600

    Clase de elemento + tipoDePregunta

---
+++ Karta2/src/karta2/PreguntaRespuesta.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karta2;
+
+public class PreguntaRespuesta {
+    Preguntas TipoPregunta;
+    boolean Es_Correcta;
+    String ResUsuario;
+    boolean Es_Respuesta;
+    Enum.Dificultad Dificultad;
+    
+}
