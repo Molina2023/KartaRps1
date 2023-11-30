dda807095d61794b8d917f21e51980a162bddde2
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-29 23:54:21 -0600

    Aqui hagan los métodos para guardar

---
+++ Karta2/src/karta2/Guardar.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karta2;
+
+import java.sql.Connection;
+
+public class Guardar {
+    /*
+    Pool GuardarGet = new Pool();
+    
+    public void Usuario (String nombreUsuario, String contraseña, int medallas_ORO, 
+            int medallas_PLATA, int medallas_BRONCE, String logros){
+        
+        Connection con =null;
+        String  SSQL = "INSERT INTO contacto (nombreUsuario, contraseña, medallas_ORO, "
+                + "medallas_PLATA, medallas_BRONCE, logros)"
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        
+        try {
+            con = GuardarGet.dataSource.getConnection();
+        } catch (SQLException e) {
+        }
+        
+    }
+*/
+}
