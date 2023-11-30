a8657ad1378d8612c020a85240cee3dd69e19167
nisit <nisit@ElHipopotamitoMicroscopico>
2023-11-29 23:59:21 -0600

    blablabla

--- Karta2/src/Front/InterfazIniciar.java
+++ Karta2/src/Front/InterfazIniciar.java
@@ -73,6 +73,7 @@ public class InterfazIniciar extends javax.swing.JFrame {
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 830, 480));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void lblBtnIniciarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBtnIniciarMouseEntered
